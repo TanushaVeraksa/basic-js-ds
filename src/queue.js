15d39decb9b0c3e881ad8f2e8b05f26d1a8213aa
tanushaveraksa <veraksa161@gmail.com>
2022-05-01 08:20:10 +0300

    feat: create queue

--- src/queue.js
+++ src/queue.js
@@ -13,22 +13,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class Queue {
+  constructor() {
+    this.queue = [];
+    this.obj = {}
   }
+ size() {
+    for(let i = 0; i<this.queue.length; i++) {
+       this.obj.value = this.queue[i]
+       this.obj.next = { value: this.queue.shift(), next:this.queue.shift() }
+    }
+    return this.obj;
+ }
+ getUnderlyingList() {
+   return this.size();
+ }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ enqueue(value) {
+    this.queue.push(value);
+ }
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ dequeue() {
+    return this.queue.shift();
+ }
 }
 
 module.exports = {
